c89ba7b46175a8c134616920244e50a3433822b8
rishabram <148410206+rishabram@users.noreply.github.com>
2024-08-12 23:51:46 -0600

    Update NonTerminal.java

--- RandomPhraseGenerator/NonTerminal.java
+++ RandomPhraseGenerator/NonTerminal.java
@@ -3,20 +3,37 @@ package comprehensive;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * This class represents a non-terminal in a grammar.
+ *
+ * @author Rishab Ramalingam and Josh Greenbaum
+ * @version 8/1/2024
+ */
 public class NonTerminal {
     private ArrayList<Phrase> phrases;
     private Random rng;
 
+    /**
+     * Constructs a NonTerminal object.
+     */
     public NonTerminal() {
         phrases = new ArrayList<>();
         rng = new Random();
     }
 
+    /**
+     * Prints a random phrase from the list of phrases.
+     */
     public void print() {
         int index = rng.nextInt(phrases.size());
         phrases.get(index).print();
     }
 
+    /**
+     * Adds a phrase to the list of phrases.
+     *
+     * @param phrase the phrase to add
+     */
     public void add(Phrase phrase) {
         phrases.add(phrase);
     }
